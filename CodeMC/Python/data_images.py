0fb8d862e2eb45635db5c6a792fa8eef22e295d2
Maxime Cauté <maxime.caute@ens-rennes.fr>
2020-05-13 09:36:50 +0200

    Added functions descriptions

--- CodeMC/Python/data_images.py
+++ CodeMC/Python/data_images.py
@@ -10,6 +10,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def request_image(situation, image_size):
+    """
+    Creates the image of the request map out of a situation.
+    This image contains a 1 for the pickup, -1 for the other cases.
+    ---
+    Input:
+        - situation:  int list list. The description of the situation,
+            with the request as a first element, and the vehicles locations afterwards.
+    Parameters:
+        - image_size: int. The size of the image.
+    Output:
+        - image: int 2D-matrix. The map of the request: 1 for the pickup, -1 for the other cases.
+    """
     image = - np.ones((image_size, image_size))
 
     request = situation[0]
@@ -23,6 +35,18 @@ def request_image(situation, image_size):
     return image
 
 def vehicles_image(situation, image_size):
+    """
+    Creates the image of the vehicles map out of a situation.
+    This image contains a 1 for the occupied cells, -1 for the others.
+    ---
+    Input:
+        - situation:  int list list. The description of the situation,
+            with the request as a first element, and the vehicles locations afterwards.
+    Parameters:
+        - image_size: int. The size of the image.
+    Output:
+        - image: int 2D-matrix. The map of the vehicles: 1 for the occupied cells, -1 for the others.
+    """
     image = -np.ones((image_size, image_size))
 
     vehicles =  situation[1:]
@@ -35,6 +59,18 @@ def vehicles_image(situation, image_size):
     return image
 
 def situation_to_2_channels(situation, image_size):
+    """
+    Creates a 2-channels image set to describe a situation.
+    The first image is the request map, the second the vehicles map
+    ---
+    Input:
+        - situation:  int list list. The description of the situation,
+            with the request as a first element, and the vehicles locations afterwards.
+    Parameters:
+        - image_size: int. The size of the image.
+    Output:
+        - data: int 3D-matrix. The 2-channels image set to describe the situation.
+    """
     data = []
     request = request_image(situation, image_size)
     data.append(request)
@@ -45,11 +81,33 @@ def situation_to_2_channels(situation, image_size):
     return data
 
 def situation_table_to_2_channels(table, image_size):
+    """
+    Creates a table of 2-channels image sets from a situation table.
+    ---
+    Input:
+        - table:  int list list list. A table containing situations description.
+    Parameters:
+        - image_size: int. The size of the image.
+    Output:
+        - data_table: int 3D-matrix list. The table containing the 2-channels image sets
+        from the situations descriptions in table.
+    """
     data_table = [situation_to_2_channels(situation, image_size) for situation in table]
 
     return data_table
 
 def assignement_image(assignement, image_size):
+    """
+    Creates the image of the assignement map out of an assignement.
+    This image contains a 1 for the assigned cell, -1 for the others.
+    ---
+    Input:
+        -assignement:  int list list. The description of the assignement, i.e. its coordinates.
+    Parameters:
+        - image_size: int. The size of the image.
+    Output:
+        - image: int 2D-matrix. The map of the assignement: 1 for the occupied cells, -1 for the others.
+    """
     image = np.zeros((image_size, image_size))
 
     x_assignement, y_assignement = assignement[0], assignement[1]
@@ -59,9 +117,28 @@ def assignement_image(assignement, image_size):
     return image
 
 def assignement_table_to_image(table, image_size):
+    """
+    Creates a table of assignement images from an assignement table.
+    ---
+    Input:
+        - table:  int list list. A table containing assignements description.
+    Parameters:
+        - image_size: int. The size of the image.
+    Output:
+        - data: int 3D-matrix. The table containing the assignement images
+        from the assignements description in table.
+    """
     data_table = [assignement_image(assignement, image_size) for assignement in table]
     return data_table
 
 def visualize_image(image , title = ""):
+    """
+    Displays an image.
+    ---
+    Input:
+        - image: int 2D-matrix. the image to be displayed.
+    Parameters:
+        - title: string. An eventual title to be displayed.
+    """
     plt.matshow(image, cmap = 'coolwarm')
     plt.title(title)
